cbe2caa09caf392e95940dc0eb54230794b3620c
MarieV <marie_vts@outlook.fr>
2020-07-11 11:31:38 +0200

    add documentation to code

--- Assets/Scripts/ARTapToPlaceObject.cs
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
+/// <summary>
+/// Place an object in the world where the user has touched the screen
+/// </summary>
 [RequireComponent(typeof(ARRaycastManager))]
 public class ARTapToPlaceObject : MonoBehaviour
 {
+    /// <summary>
+    /// Object to instantiate when we place an object
+    /// </summary>
     public GameObject gameObjectToInstantiate;
 
+
+    /// <summary>
+    /// Object that has been placed in the world
+    /// </summary>
     private GameObject spawnedObject;
     private ARRaycastManager _arRaycastManager;
+    /// <summary>
+    /// Position of the touch input
+    /// </summary>
     private Vector2 touchPosition;
 
+    /// <summary>
+    /// List of ARRaycastHit used to get hit results from the ARRaycastManager
+    /// </summary>
     static List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     private void Awake()
@@ -20,6 +36,13 @@ public class ARTapToPlaceObject : MonoBehaviour
         _arRaycastManager = GetComponent<ARRaycastManager>();
     }
 
+    /// <summary>
+    /// Check if there is a touch input.
+    /// If yes <paramref name="touchPosition"/> gets its position.
+    /// If not, <paramref name="touchPosition"/> is set to its default value (0, 0).
+    /// </summary>
+    /// <param name="touchPosition">position of the touch input, this object is modified by the function</param>
+    /// <returns>True if there is a touch input, false otherwise</returns>
     bool TryGetTouchPosition(out Vector2 touchPosition)
     {
         if (Input.touchCount > 0)
@@ -37,6 +60,7 @@ public class ARTapToPlaceObject : MonoBehaviour
             return;
         if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
         {
+            // If touch input and raycast hit a plane, then spawn an object or move the existing spawned object.
             var hitPose = hits[0].pose;
 
             if (spawnedObject == null)
