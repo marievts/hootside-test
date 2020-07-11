7084341d1ba7070550d573373a41afa0fbc00d17
MarieV <marie_vts@outlook.fr>
2020-07-11 15:18:17 +0200

    add content to menu

--- Assets/Scripts/HandleMenu.cs
+++ Assets/Scripts/HandleMenu.cs
@@ -17,18 +17,6 @@ public class HandleMenu : MonoBehaviour
     /// </summary>
     private bool isOpen = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     /// <summary>
     /// Toogle the menu
     /// </summary>
