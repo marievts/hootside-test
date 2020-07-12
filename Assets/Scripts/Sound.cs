b9119f54815fd6c39fc7b4c6c5fd3f06889d9f9a
MarieV <marie_vts@outlook.fr>
2020-07-12 10:53:24 +0200

    Ok I'm dumb, every sounds pitch values were at 0, so that was normal we couldn't hear a thing... :woman_facepalming:

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,18 +1,39 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Represent a sound to be played within the app.
+/// </summary>
 [System.Serializable]
 public class Sound
 {
+    /// <summary>
+    /// Name that sound should be refered as.
+    /// </summary>
     public string name;
 
+    /// <summary>
+    /// Sound file to be played
+    /// </summary>
     public AudioClip clip;
 
+    /// <summary>
+    /// Volume the sound must be played
+    /// </summary>
     [Range(0f, 3f)]
     public float volume = 1f;
+    /// <summary>
+    /// Pitch the sound must be played
+    /// </summary>
     [Range(.1f, 3f)]
-    public float pitch;
+    public float pitch = 1f;
+    /// <summary>
+    /// Must this sound be played in loop ?
+    /// </summary>
     public bool loop;
 
+    /// <summary>
+    /// AudioSource playing the sound
+    /// </summary>
     [HideInInspector]
     public AudioSource source;
 }
